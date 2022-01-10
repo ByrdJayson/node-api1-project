1b7d442512baa139bf498659c4516bc7dd7dea53
Jayson Byrd <jayson_byrd_95@outlook.com>
2022-01-10 17:44:09 -0500

    All Tests Passing and Endpoints Functional

--- api/server.js
+++ api/server.js
@@ -62,4 +62,22 @@ server.delete('/api/users/:id', async (req, res) => {
     }
 })
 
+//PUT REQUESTS
+server.put('/api/users/:id', async (req, res) => {
+    try {
+        const { id } = req.params
+        const { name, bio } = req.body
+        const user = await Users.update(id, {name, bio})
+        if(!user) {
+            return res.status(404).json({message: "The user with the specified ID does not exist"})
+        } else if(!name || !bio){
+            return res.status(400).json({message: "Please provide name and bio for the user"})
+        } else{
+            res.status(200).json(user)
+        }
+    } catch(err){
+        res.status(500).json({message: "The user information could not be modified"})
+    }
+})
+
 module.exports = server; // EXPORT YOUR SERVER instead of {}
