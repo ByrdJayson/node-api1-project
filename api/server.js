221bef1e0f48453bca09fafc946194c4f153ff3f
Jayson Byrd <jayson_byrd_95@outlook.com>
2022-01-10 17:07:59 -0500

    Fetching user functioning

--- api/server.js
+++ api/server.js
@@ -6,6 +6,8 @@ const server = express()
 
 server.use(express.json())
 
+
+//GET REQUESTS
 server.get('/api/users', async (req, res ) => {
     try {
         const users = await Users.find()
@@ -14,6 +16,20 @@ server.get('/api/users', async (req, res ) => {
         res.status(500).json({message: err.message})
     }
 
+})
+
+server.get('/api/users/:id', async (req, res) => {
+    try {
+        const { id } = req.params
+        const user = await Users.findById(id)
+        if(!user){
+            res.status(404).json({message: "The user with the specified ID does not exist"})
+        } else {
+            res.status(200).json(user)
+        }
+    } catch(err){
+        res.status(500).json({message: err.message})
+    }
 })
 
 
