331134d94918736755396060a7a7479da741fad9
Jayson Byrd <jayson_byrd_95@outlook.com>
2022-01-10 17:17:42 -0500

    Posting new users mostly functional

--- api/server.js
+++ api/server.js
@@ -32,5 +32,19 @@ server.get('/api/users/:id', async (req, res) => {
     }
 })
 
+//POST REQUESTS
+server.post('/api/users', async (req, res) => {
+    try {
+        const { name, bio } = req.body
+        const newUser = await Users.insert({name, bio})
+
+        if(!req.body.name || !req.body.bio) {
+            res.status(400).json({message: "Please provide name and bio for the user"})
+        }
+        res.status(201).json(newUser)
+    } catch(err) {
+        res.status(500).json({message: err.message})
+    }
+})
 
 module.exports = server; // EXPORT YOUR SERVER instead of {}
